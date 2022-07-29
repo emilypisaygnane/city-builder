82647359620fa23d7b75606c86e0eac712819673
Emily <episaygnane@me.com>
2022-07-28 18:55:12 -0700

    All HTML, dropdowns, pictures and text box

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
 // import functions and grab DOM elements
+const waterDropdownEl = document.getElementById('water-dropdown');
+const tropicalDropdownEl = document.getElementById('tropical-dropdown');
+const snowyDropdownEl = document.getElementById('snowy-dropdown');
 
+const waterImgTag = document.getElementById('water-img');
+const tropicalImgTag = document.getElementById('tropical-img');
+const snowyImgTag = document.getElementById('snowy-img');
+
+const timesChangedEl = document.getElementById('time-changed');
+const sloganInputEl = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
 // let state
+let timesWaterChanged = 0;
+let timesTropicalChanged = 0;
+let timesSnowyChanged = 0;
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+const slogansEl = document.getElementById('slogans');
+
+sloganButton.addEventListener('click', () => {
+    const newSlogan = sloganInputEl.value;
+    slogans.push(newSlogan);
+    sloganInputEl.value = '';
+    displayslogans();
+});
+
+waterDropdownEl.addEventListener('change', () => {
+    waterImgTag.src = `assets/${waterDropdownEl.value}.PNG`;
+    timesWaterChanged++;
+    displayStats();
+});
+tropicalDropdownEl.addEventListener('change', () => {
+    tropicalImgTag.src = `assets/${tropicalDropdownEl.value}.PNG`;
+    timesTropicalChanged++;
+    displayStats();
+});
+snowyDropdownEl.addEventListener('change', () => {
+    snowyImgTag.src = `assets/${snowyDropdownEl.value}.PNG`;
+    timesSnowyChanged++;
+    displayStats();
+});
+
+function displayStats() {
+    timesChangedEl.textContent = `you've changed the water destination ${timesWaterChanged} times, the tropical destination ${timesTropicalChanged} times, and the snowy destination ${timesSnowyChanged} times.`;
+}
+
+function displaySlogans() {
+    slogansEl.textContent = '';
+
+    for (let slogan of slogan) {
+        const sloganEl = document.createElement('p');
+        sloganEl.textContent = slogan;
+        sloganEl.classList.add('slogan');
+        slogansEl.append(sloganEl);
+    }
+}
+
+displaySlogans();
