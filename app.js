ded3a45f9826473574f80754c8ff4f5bc36eb484
Emily <episaygnane@me.com>
2022-07-28 19:29:49 -0700

    ALL JavsScript, and a little CSS

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const waterImgTag = document.getElementById('water-img');
 const tropicalImgTag = document.getElementById('tropical-img');
 const snowyImgTag = document.getElementById('snowy-img');
 
-const timesChangedEl = document.getElementById('time-changed');
+const timesChangedEl = document.getElementById('times-changed');
 const sloganInputEl = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
 // let state
 let timesWaterChanged = 0;
 let timesTropicalChanged = 0;
 let timesSnowyChanged = 0;
+let slogans = [];
 
 // set event listeners
 const slogansEl = document.getElementById('slogans');
@@ -22,33 +23,35 @@ sloganButton.addEventListener('click', () => {
     const newSlogan = sloganInputEl.value;
     slogans.push(newSlogan);
     sloganInputEl.value = '';
-    displayslogans();
+    displaySlogans();
 });
 
 waterDropdownEl.addEventListener('change', () => {
     waterImgTag.src = `assets/${waterDropdownEl.value}.PNG`;
     timesWaterChanged++;
-    displayStats();
+    displayCountStats();
 });
+
 tropicalDropdownEl.addEventListener('change', () => {
     tropicalImgTag.src = `assets/${tropicalDropdownEl.value}.PNG`;
     timesTropicalChanged++;
-    displayStats();
+    displayCountStats();
 });
+
 snowyDropdownEl.addEventListener('change', () => {
     snowyImgTag.src = `assets/${snowyDropdownEl.value}.PNG`;
     timesSnowyChanged++;
-    displayStats();
+    displayCountStats();
 });
 
-function displayStats() {
-    timesChangedEl.textContent = `you've changed the water destination ${timesWaterChanged} times, the tropical destination ${timesTropicalChanged} times, and the snowy destination ${timesSnowyChanged} times.`;
-}
+function displayCountStats() {
+    timesChangedEl.textContent = `You have changed your water destination ${timesWaterChanged} times, your tropical destination ${timesTropicalChanged} times, and your snowy destinations ${timesSnowyChanged} times. It's best to make up your mind!`;
+} 
 
 function displaySlogans() {
     slogansEl.textContent = '';
 
-    for (let slogan of slogan) {
+    for (let slogan of slogans) {
         const sloganEl = document.createElement('p');
         sloganEl.textContent = slogan;
         sloganEl.classList.add('slogan');
